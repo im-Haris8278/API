33a783704b9860db8383c35f4141c8469cdbf683
Haris Baig <115177903+im-Haris8278@users.noreply.github.com>
2023-03-07 20:23:03 +0500

    REST_API

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+const apiData = require("./data.json");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/service", (req, res) => {
+  res.send(apiData);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on Port ${port}`);
+});
